43112e28eb3af3efab06830c009e659819c4732a
RemixDev <RemixDev64@gmail.com>
2021-07-25 13:59:26 +0200

    v3.3.3

--- deemix/__init__.py
+++ deemix/__init__.py
@@ -11,7 +11,7 @@ from deemix.itemgen import generateTrackItem, \
     LinkNotRecognized, \
     LinkNotSupported
 
-__version__ = "3.3.2"
+__version__ = "3.3.3"
 
 # Returns the Resolved URL, the Type and the ID
 def parseLink(link):

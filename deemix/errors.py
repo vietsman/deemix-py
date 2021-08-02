2e7c5c4e658fc014056b076d4491d18c51e3cb6c
RemixDev <RemixDev64@gmail.com>
2021-08-02 23:08:01 +0200

    Code parity between libraries

--- deemix/errors.py
+++ deemix/errors.py
@@ -53,12 +53,14 @@ ErrorMessages = {
     'notEncodedNoAlternative': "Track not yet encoded and no alternative found!",
     'wrongBitrate': "Track not found at desired bitrate.",
     'wrongBitrateNoAlternative': "Track not found at desired bitrate and no alternative found!",
-    'wrongLicense': "Your account can't stream the track at the desired bitrate",
+    'wrongLicense': "Your account can't stream the track at the desired bitrate.",
     'no360RA': "Track is not available in Reality Audio 360.",
     'notAvailable': "Track not available on deezer's servers!",
     'notAvailableNoAlternative': "Track not available on deezer's servers and no alternative found!",
     'noSpaceLeft': "No space left on target drive, clean up some space for the tracks",
-    'albumDoesntExists': "Track's album does not exsist, failed to gather info"
+    'albumDoesntExists': "Track's album does not exsist, failed to gather info.",
+    'notLoggedIn': "You need to login to download tracks.",
+    'wrongGeolocation': "Your account can't stream the track from your current country."
 }
 
 class DownloadFailed(DownloadError):

203ec1f10f0d44ded00ec220a8c621f1aa8237b1
RemixDev <RemixDev64@gmail.com>
2022-01-04 22:13:54 +0100

    v3.6.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="deemix",
-    version="3.6.4",
+    version="3.6.5",
     description="A barebone deezer downloader library",
     long_description=README,
     long_description_content_type="text/markdown",
